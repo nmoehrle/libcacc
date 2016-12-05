ec11cf8e9f0f5a8cd8a1787fa401dbcad3a45524
Nils Moehrle <nils@kuenstle-moehrle.de>
2016-12-05 17:49:16 +0100

    Add texture wrappers

---
+++ array_texture.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2015, Nils Moehrle
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ */
+
+#ifndef CACC_ARRAYTEXTURE_HEADER
+#define CACC_ARRAYTEXTURE_HEADER
+
+#include "defines.h"
+
+#include <cuda_runtime_api.h>
+
+CACC_NAMESPACE_BEGIN
+
+template<typename T>
+class ArrayTexture {
+public:
+    class YAccessor {
+    private:
+        cudaTextureObject_t tex;
+        int x;
+    public:
+        __device__ __forceinline__
+        YAccessor(cudaTextureObject_t tex, int x) : tex(tex), x(x) {}
+        __device__ __forceinline__
+        T operator[](int y) {
+            return tex2D<T>(tex, x, y);
+        }
+    };
+
+    class Accessor {
+    private:
+        cudaTextureObject_t tex;
+    public:
+        Accessor(cudaTextureObject_t tex) : tex(tex) {}
+        __device__ __forceinline__
+        YAccessor operator[](int x) {
+            return YAccessor(tex, x);
+        }
+    };
+
+private:
+    cudaTextureObject_t tex;
+protected:
+    void initialize(cudaArray_t array);
+    ArrayTexture() {}
+public:
+    ArrayTexture(cudaArray_t array);
+    ~ArrayTexture();
+    Accessor accessor() { return Accessor(tex); };
+};
+
+template<typename T>
+class MappedArrayTexture : public ArrayTexture<T> {
+private:
+    cudaGraphicsResource_t res;
+public:
+    MappedArrayTexture(cudaGraphicsResource_t res);
+    ~MappedArrayTexture();
+};
+
+template<typename T>
+MappedArrayTexture<T>::MappedArrayTexture(cudaGraphicsResource_t res) : res(res) {
+    cudaArray_t array;
+    CHECK(cudaGraphicsSubResourceGetMappedArray(&array, res, 0, 0));
+    this->initialize(array);
+}
+
+template<typename T>
+ArrayTexture<T>::ArrayTexture(cudaArray *array) {
+    initialize(array);
+}
+
+template<typename T>
+ArrayTexture<T>::~ArrayTexture() {
+    CHECK(cudaDestroyTextureObject(tex));
+}
+
+template<typename T>
+MappedArrayTexture<T>::~MappedArrayTexture() {
+    CHECK(cudaGraphicsUnmapResources(1, &res));
+}
+
+template<typename T>
+void
+ArrayTexture<T>::initialize(cudaArray *array) {
+    cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType = cudaResourceTypeArray;
+    resDesc.res.array.array = array;
+
+    cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.readMode = cudaReadModeElementType;
+
+    CHECK(cudaCreateTextureObject(&tex, &resDesc, &texDesc, NULL));
+}
+
+CACC_NAMESPACE_END
+
+#endif /* CACC_ARRAYTEXTURE_HEADER */
---
+++ buffer_texture.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2015, Nils Moehrle
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ */
+
+#ifndef CACC_BUFFERTEXTURE_HEADER
+#define CACC_BUFFERTEXTURE_HEADER
+
+#include "defines.h"
+
+#include <cuda_runtime_api.h>
+
+CACC_NAMESPACE_BEGIN
+
+template<typename T>
+class BufferTexture {
+public:
+    class Accessor {
+    private:
+        cudaTextureObject_t tex;
+    public:
+        __device__ __forceinline__
+        T operator[](int idx) {
+            return tex1Dfetch<T>(tex, idx);
+        }
+    };
+
+private:
+    cudaTextureObject_t tex;
+protected:
+    void initialize(T *ptr, size_t size);
+    BufferTexture() {}
+public:
+    BufferTexture(T *ptr, size_t size);
+    ~BufferTexture();
+    Accessor accessor() { return Accessor(tex); };
+};
+
+template<typename T>
+class MappedBufferTexture : public BufferTexture<T> {
+private:
+    cudaGraphicsResource_t res;
+public:
+    MappedBufferTexture(cudaGraphicsResource_t res);
+    ~MappedBufferTexture();
+};
+
+template<typename T>
+BufferTexture<T>::BufferTexture(T *ptr, size_t size) {
+    initialize(ptr, size);
+}
+
+template<typename T>
+MappedBufferTexture<T>::MappedBufferTexture(cudaGraphicsResource_t res) {
+    T *ptr;
+    size_t size;
+    CHECK(cudaGraphicsResourceGetMappedPointer((void **)&ptr, &size, res));
+    this->initialize(ptr, size);
+}
+
+template<typename T>
+BufferTexture<T>::~BufferTexture() {
+    CHECK(cudaDestroyTextureObject(tex));
+}
+
+template<typename T>
+MappedBufferTexture<T>::~MappedBufferTexture() {
+    CHECK(cudaGraphicsUnmapResources(1, &res));
+}
+
+template<typename T>
+void
+BufferTexture<T>::initialize(T *ptr, size_t size) {
+    cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType = cudaResourceTypeLinear;
+    resDesc.res.linear.devPtr = ptr;
+    resDesc.res.linear.desc = cudaCreateChannelDesc<T>();
+    resDesc.res.linear.sizeInBytes = sizeof(T) * size;
+
+    cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.readMode = cudaReadModeElementType;
+
+    CHECK(cudaCreateTextureObject(&tex, &resDesc, &texDesc, NULL));
+}
+
+CACC_NAMESPACE_END
+
+#endif /* CACC_BUFFERTEXTURE_HEADER */
